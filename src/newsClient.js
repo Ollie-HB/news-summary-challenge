c575adf006e3e6396f9244805a375e3156cbc355
Ollie Beck <ollieb94@hotmail.co.uk>
2022-10-29 16:26:28 +0100

    Reformatting

--- src/newsClient.js
+++ src/newsClient.js
@@ -1,12 +1,14 @@
 const apiKey = require('./apiKey');
 
 const apiUrl = `https://content.guardianapis.com/search?api-key=${apiKey}`;
+
 class NewsClient {
   loadNews(callback) {
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
       callback(data);
+      console.log(data);
       })
     };
   };
--- src/newsModel.test.js
+++ src/newsModel.test.js
@@ -1,6 +1,7 @@
 const NewsModel = require('./newsModel');
 
 describe('NewsModel', () => {
+  
   it('#getNews - initialises with an empty array for news stories', () => {
     news = new NewsModel();
     expect(news.getNews()).toEqual([])
